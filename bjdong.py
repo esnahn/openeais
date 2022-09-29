65506c31c97af95a913cf76c1611aab2e68b8261
Euisoon Ahn <euisoon.ahn@gmail.com>
2022-09-29 09:45:01 +0900

    output in output dir / create sido

--- bjdong.py
+++ bjdong.py
@@ -2,16 +2,25 @@ import pandas as pd
 from tabulate import tabulate
 
 df = pd.read_csv(
-    "법정동코드 전체자료.txt", encoding="euc-kr", sep="\t", index_col=0, dtype={"법정동코드": str}
+    "data/법정동코드 전체자료.txt",
+    encoding="euc-kr",
+    sep="\t",
+    index_col=0,
+    dtype={"법정동코드": str},
 )
 print(tabulate(df.head(), headers="keys"))
-df.to_csv("code_bjd.csv")
+df.to_csv("output/code_bjd.csv")
 
 df_sgg = df.loc[df.index.str.endswith("00000")]
 df_sgg.index = df_sgg.index.str[:5]
 df_sgg.index.name = "시군구코드"
 df_sgg = df_sgg.rename(columns={"법정동명": "시군구명"})
 print(tabulate(df_sgg.head(), headers="keys"))
-df_sgg.to_csv("code_sgg.csv")
-# print(df_bjd.loc[df[0]])
-# df_bjd.loc[df[0]]["법정동명"].to_csv("bjd_output.txt", index=False, header=False)
+df_sgg.to_csv("output/code_sgg.csv")
+
+df_sido = df.loc[df.index.str.endswith("00000000")]
+df_sido.index = df_sido.index.str[:2]
+df_sido.index.name = "시도코드"
+df_sido = df_sido.rename(columns={"법정동명": "시도명"})
+print(tabulate(df_sido.head(), headers="keys"))
+df_sido.to_csv("output/code_sido.csv")
