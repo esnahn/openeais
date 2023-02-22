8b3022842ca3b2ab26429ae7cec26c866411f17e
Euisoon Ahn <euisoon.ahn@gmail.com>
2023-02-22 17:51:26 +0900

    change output dir of bjdong (into data folder)

--- bjdong.py
+++ bjdong.py
@@ -9,18 +9,18 @@ df = pd.read_csv(
     dtype={"법정동코드": str},
 )
 print(tabulate(df.head(), headers="keys"))
-df.to_csv("output/code_bjd.csv")
+df.to_csv("data/code_bjd.csv")
 
 df_sgg = df.loc[df.index.str.endswith("00000")]
 df_sgg.index = df_sgg.index.str[:5]
 df_sgg.index.name = "시군구코드"
 df_sgg = df_sgg.rename(columns={"법정동명": "시군구명"})
 print(tabulate(df_sgg.head(), headers="keys"))
-df_sgg.to_csv("output/code_sgg.csv")
+df_sgg.to_csv("data/code_sgg.csv")
 
 df_sido = df.loc[df.index.str.endswith("00000000")]
 df_sido.index = df_sido.index.str[:2]
 df_sido.index.name = "시도코드"
 df_sido = df_sido.rename(columns={"법정동명": "시도명"})
 print(tabulate(df_sido.head(), headers="keys"))
-df_sido.to_csv("output/code_sido.csv")
+df_sido.to_csv("data/code_sido.csv")
