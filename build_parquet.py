34780fbdb18da2682ac63bd33002f1177aaaa5d2
Euisoon Ahn <euisoon.ahn@gmail.com>
2023-05-09 21:39:04 +0900

    set index on first column

--- build_parquet.py
+++ build_parquet.py
@@ -3,4 +3,4 @@ from pathlib import Path
 from openeais import openeais_to_parquet
 
 for file in Path("data/").glob("국토교통부_*.zip"):
-    openeais_to_parquet(file)
+    openeais_to_parquet(file, index=0)
